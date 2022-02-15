ee35e01e3b1c2955596d5e077e2740189986dbe6
javi <jcortinaf@gmail.com>
2022-02-15 19:44:56 +0100

    fix intentional error

--- app/main.py
+++ app/main.py
@@ -29,4 +29,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my API!! Successfully deployed from CI/CD pipeline"}
+    return {"message": "Welcome to my API!! Successfully deployed from CI/CD pipeline :D"}
